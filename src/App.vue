3a98de89ca9fcc85ec5701188dcfb54bac49e920
shb <2269629396@qq.com>
2019-04-01 10:48:42 +0800

    项目初始化配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,10 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  overflow-x: hidden;
+  min-height: 100vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,46 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import FastClick from 'fastclick'; // 引入FastClick
+import HTTP from './lib/httpRequest';
+import API from './lib/api';
+import './config/rem';
+import '@/style/reset.less'
+import { Button, Toast } from 'vant';
+[Button, Toast].map(item => {
+  Vue.use(item);
+});
+Vue.config.productionTip = false;
+Vue.prototype.$HTTP = HTTP;
+Vue.prototype.$API = API;
+// 提示框
+Vue.prototype.$Tip = msg =>
+  Toast({
+    message: msg,
+    forbidClick: true,
+    duration: 1500
+  });
+// 加载提示
+Vue.prototype.$Loading = msg =>
+  Toast.loading({
+    message: msg,
+    forbidClick: true,
+    duration: 0
+  });
 
-Vue.config.productionTip = false
+// 使用 fastclick
+FastClick.attach(document.body);
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
-})
+  template: '<App/>',
+  mounted() {
+    document.dispatchEvent(new Event('render-event'));
+  }
+});
